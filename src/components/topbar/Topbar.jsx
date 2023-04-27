5bcafd75ed2f239f3b0fc053320142fd707f19b2
sevinyay <you@example.com>
2023-04-27 23:59:00 +0300

    barrr

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.css";
 import { Person, Mail} from "@mui/icons-material";
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className="topbar active">
+    <div className= {"topbar " + (menuOpen && "active")}>
     <div className="wrapper"> 
     <div className="left"> 
     <a href="#intro" className="logo"> boradek </a>
@@ -17,7 +17,7 @@ export default function Topbar() {
     </div>
     </div>
     <div className="right"> 
-    <div className="hamburger"> 
+    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}> 
     <span className="line1"> </span>
     <span className="line2"> </span>
     <span className="line3"> </span>
